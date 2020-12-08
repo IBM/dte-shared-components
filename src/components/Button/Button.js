7a1cdd833912a86dc036f922f08c647d304f875a
Christian Rosado <christianrosadoibm@Christians-MacBook-Pro.local>
2020-12-08 17:05:38 -0400

    uploading code

---
+++ src/components/Button/Button.js
@@ -0,0 +1,17 @@
+// DTE 2.0 Button
+import React from "react";
+// import { Button as CarbonButton } from "carbon-components-react";
+
+const Button = ({title}) => {
+//   return <CarbonButton {...props} />;
+    return <button className="testingBTN">{title}</button>
+};
+
+Button.defaultProps = {
+  size: "default",
+  tooltipPosition: "top",
+  tooltipAlignment: "center",
+  kind: "primary",
+};
+
+export default Button;
